b49087926c6fda10458a610629c052b347883491
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-06-25 11:27:59 +0700

    Call Api Successfully with getStaticProps 25/6/2022

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,39 @@
-function Home(){
-    return <h1>Home Page</h1>
+function Home({ data }) {
+  console.log(data);
+  return (
+    <>
+      <h1>Home Page</h1>
+      {data.map(function (item, idx) {
+        return (
+          <>
+            <p>{item.name}</p>
+            <p>{item.email}</p>
+          </>
+        );
+      })}
+    </>
+  );
 }
 
-export default Home
+export default Home;
+export async function getStaticProps() {
+  // fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then(res=>res.json())
+  //     .then(item=>{
+  //         return {
+  //             props: {
+  //               data: item,
+  //             },
+  //           };
+  //     })
+  //     .catch(e=>{
+  //         console.log(e)
+  //     })
+  const respone = await fetch("https://jsonplaceholder.typicode.com/users");
+  const resutl = await respone.json();
+  return {
+    props: {
+      data: resutl,
+    },
+  };
+}
